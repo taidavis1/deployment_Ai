bb316e073747f797e345fe728d5497edf936d7ca
Levan Fuentes-Parra <levanfuentes-parra@Levans-Air.attlocal.net>
2023-04-26 03:30:23 -0700

    changes

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask , render_template , redirect , url_for
-
-app = Flask(__name__ , static_folder= 'staitc' , template_folder= 'templates')
-
-
-################################################
+from flask import Flask , render_template , redirect , url_for, request, jsonify
+import backend
+app = Flask(__name__ , static_folder= 'static' , template_folder= 'templates')
+@app.route('/' , methods = ['GET' , 'POST'])
+def Home():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        input1 = request.args.get('input1')
+        input2 = request.args.get('input2')
+        input3 = request.args.get('input3')
+        input4 = request.args.get('input4')
+        predict = backend.predictions(input1 , input2 , input3 , input4)
+        print(predict)
+        return "Hello"
+###################################################
 if __name__ == '__main__':
-    app.run(host='127.0.0.1' , port=3306 , debug=True)
+    app.run(host='127.0.0.1' , port=8000 , debug=True)
